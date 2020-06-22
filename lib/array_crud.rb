b8e72d6a3a7ff7593efd888912a9212eb2bb9172
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-22 00:05:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  colors = ["Red", "Orange", "Yellow", "Green"]
+  colors << ("Blue")
 end
 
 def add_element_to_start_of_array(array, element)
