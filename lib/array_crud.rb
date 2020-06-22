e3077210fff7883b1ddb0670e2e026fef8c169d4
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-22 00:10:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   drinks = ["Water", "Milk", "Tea", "Wine"]
-  colors << "Beer"
+  drinks << "Beer"
 end
 
 def add_element_to_start_of_array(array, element)
