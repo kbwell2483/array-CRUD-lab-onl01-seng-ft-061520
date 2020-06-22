0691c709cc8ba6a0e61d7cd5b7eb73cd0d18452f
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-22 01:01:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  transport = ["car", "bus", "train"]
+  transport = ["am", "bus", "train"]
   transport = [0]
 end
 
